08c183c203a807a3564c74641cdd34ccea7ba6c4
AllDayDev0x <whitestar0713@gmail.com>
2024-07-07 03:18:33 -0700

    change modal

--- app/components/TokenModal.tsx
+++ app/components/TokenModal.tsx
@@ -37,7 +37,7 @@ export default function TokenModal({
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative flex flex-col transform overflow-hidden rounded-lg bg-black border h-[700px] max-h-[90vh] bg-opacity-90 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
+            className="relative mb-5 flex flex-col transform overflow-hidden rounded-lg bg-black border h-[800px] max-h-[90vh] bg-opacity-90 p text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
             <div className="flex-1 overflow-hidden flex flex-col mt-3 px-4 mx-3 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="text-centers sm:text-left">
                 <DialogTitle as="h3" className="text-white font-semibold leading-6 text-2xl">
@@ -70,21 +70,7 @@ export default function TokenModal({
                 ))}
               </div>
             </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button
-                type="button"
-                onClick={() => setOpen(false)}
-                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">
-                Deactivate
-              </button>
-              <button
-                type="button"
-                data-autofocus
-                onClick={() => setOpen(false)}
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">
-                Cancel
-              </button>
-            </div>
+            <div className="mb-10" />
           </DialogPanel>
         </div>
       </div>
